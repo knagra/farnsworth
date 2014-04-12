3800cda77cbe81100fb259f0081889029f1ea460
Nader Morshed <morshed.nader@gmail.com>
2014-04-11 19:58:59 -0700

    Improved the post messages form

--- static/tinymce/threads.js
+++ static/tinymce/threads.js
@@ -13,8 +13,9 @@ tinyMCE.init({
 tinyMCE.init({
 	selector: "textarea.message",
 	theme: "modern",
-	height: 100,
-	width: 640,
+	height: 200,
+	width: "100%",
+	resize: "both",
 	plugins: "paste searchreplace emoticons link image textcolor charmap media preview",
 	toolbar1: "styleselect | fontselect | fontsizeselect | bullist numlist | outdent indent hr | undo redo",
 	toolbar2: "bold italic underline | cut copy paste pastetext pasteword | search replace | link unlink charmap | emoticons image media | forecolor backcolor | preview",
