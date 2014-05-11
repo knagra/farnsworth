85dc58853636d37a82d3dad2c64660a7d4e5dea7
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-05-10 22:55:55 -0700

    progress on help page, started writing tests

--- threads/tests.py
+++ threads/tests.py
@@ -6,7 +6,8 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from models import UserProfile
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +15,11 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class VerifyUser(TestCase):
+	def test_user_profile_created(self):
+		''' Test that the user profile for a user is automatically created when a user is created. '''
+		new_user = User.objects.create_user(username="user", email="fake@email.com", first_name="john", last_name="doe", password="password")
+		new_user.save()
+		self.assertNotEqual(0, UserProfile.objects.filter(user=new_user).count())
+		self.assertEqual(new_user, UserProfile.objects.get(user=new_user).user)
