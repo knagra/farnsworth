7d41e05703d516c2e2cf05009b7e36b7b48f5e00
Nader Morshed <morshed.nader@gmail.com>
2014-05-11 10:57:51 -0700

    Changed the user passwords Added a test for a single request page

--- managers/tests.py
+++ managers/tests.py
@@ -9,15 +9,15 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from farnsworth.settings import MESSAGES
 from threads.models import UserProfile
-from managers.models import ProfileRequest, Manager, RequestType
+from managers.models import ProfileRequest, Manager, RequestType, Request
 
 class ManagementPermission(TestCase):
 	def setUp(self):
-		self.u = User.objects.create_user(username="u", email="u@email.com", password="password")
-		self.st = User.objects.create_user(username="st", email="st@email.com", password="password")
-		self.pu = User.objects.create_user(username="pu", email="su@email.com", password="password")
-		self.su = User.objects.create_user(username="su", email="su@email.com", password="password")
-		self.np = User.objects.create_user(username="np", email="np@email.com", password="password")
+		self.u = User.objects.create_user(username="u", email="u@email.com", password="pwd")
+		self.st = User.objects.create_user(username="st", email="st@email.com", password="pwd")
+		self.pu = User.objects.create_user(username="pu", email="su@email.com", password="pwd")
+		self.su = User.objects.create_user(username="su", email="su@email.com", password="pwd")
+		self.np = User.objects.create_user(username="np", email="np@email.com", password="pwd")
 		self.st.is_staff = True
 		self.su.is_staff, self.su.is_superuser = True, True
 		self.u.save()
@@ -36,33 +36,38 @@ class ManagementPermission(TestCase):
 		food.managers = [president]
 		food.save()
 
+		self.request = Request(owner=UserProfile.objects.get(user=self.u),
+				       body="request body", request_type=food)
+		self.request.save()
+
 		UserProfile.objects.get(user=self.np).delete()
-		self.pr = ProfileRequest(username="pr", email="pr@email.com", affiliation=UserProfile.STATUS_CHOICES[0][0])
+		self.pr = ProfileRequest(username="pr", email="pr@email.com",
+					 affiliation=UserProfile.STATUS_CHOICES[0][0])
 		self.pr.save()
 
 	def _admin_required(self, url, success_target=None):
 		response = self.client.get(url)
 		self.assertRedirects(response, "/login/")
 
-		self.client.login(username="np", password="password")
+		self.client.login(username="np", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/landing/")
 		self.assertIn(MESSAGES["NO_PROFILE"], response.content)
 		self.client.logout()
 
-		self.client.login(username="u", password="password")
+		self.client.login(username="u", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/")
 		self.assertIn(MESSAGES["ADMINS_ONLY"], response.content)
 		self.client.logout()
 
-		self.client.login(username="st", password="password")
+		self.client.login(username="st", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/")
 		self.assertIn(MESSAGES["ADMINS_ONLY"], response.content)
 		self.client.logout()
 
-		self.client.login(username="su", password="password")
+		self.client.login(username="su", password="pwd")
 		response = self.client.get(url)
 		if success_target is None:
 			self.assertEqual(response.status_code, 200)
@@ -74,25 +79,25 @@ class ManagementPermission(TestCase):
 		response = self.client.get(url)
 		self.assertRedirects(response, "/login/")
 
-		self.client.login(username="np", password="password")
+		self.client.login(username="np", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/landing/")
 		self.assertIn(MESSAGES["NO_PROFILE"], response.content)
 		self.client.logout()
 
-		self.client.login(username="u", password="password")
+		self.client.login(username="u", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/")
 		self.assertIn(MESSAGES["PRESIDENTS_ONLY"], response.content)
 		self.client.logout()
 
-		self.client.login(username="st", password="password")
+		self.client.login(username="st", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/")
 		self.assertIn(MESSAGES["PRESIDENTS_ONLY"], response.content)
 		self.client.logout()
 
-		self.client.login(username="su", password="password")
+		self.client.login(username="su", password="pwd")
 		response = self.client.get(url)
 		if success_target is None:
 			self.assertEqual(response.status_code, 200)
@@ -100,7 +105,7 @@ class ManagementPermission(TestCase):
 			self.assertRedirects(response, success_target)
 		self.client.logout()
 
-		self.client.login(username="pu", password="password")
+		self.client.login(username="pu", password="pwd")
 		response = self.client.get(url)
 		if success_target is None:
 			self.assertEqual(response.status_code, 200)
@@ -112,13 +117,13 @@ class ManagementPermission(TestCase):
 		response = self.client.get(url)
 		self.assertRedirects(response, "/login/")
 
-		self.client.login(username="np", password="password")
+		self.client.login(username="np", password="pwd")
 		response = self.client.get(url, follow=True)
 		self.assertRedirects(response, "/landing/")
 		self.assertIn(MESSAGES["NO_PROFILE"], response.content)
 		self.client.logout()
 
-		self.client.login(username="u", password="password")
+		self.client.login(username="u", password="pwd")
 		response = self.client.get(url, follow=True)
 		if success_target is None:
 			self.assertEqual(response.status_code, 200)
@@ -126,7 +131,7 @@ class ManagementPermission(TestCase):
 			self.assertRedirects(response, success_target)
 		self.client.logout()
 
-		self.client.login(username="st", password="password")
+		self.client.login(username="st", password="pwd")
 		response = self.client.get(url, follow=True)
 		if success_target is None:
 			self.assertEqual(response.status_code, 200)
@@ -134,7 +139,7 @@ class ManagementPermission(TestCase):
 			self.assertRedirects(response, success_target)
 		self.client.logout()
 
-		self.client.login(username="su", password="password")
+		self.client.login(username="su", password="pwd")
 		response = self.client.get(url, follow=True)
 		if success_target is None:
 			self.assertEqual(response.status_code, 200)
@@ -181,15 +186,14 @@ class ManagementPermission(TestCase):
 			"archives/all_requests",
 			"requests/food/all",
 			"my_requests",
-			# "request/request_pk",
+			"request/{0}".format(self.request.pk),
 			"archives/all_announcements",
 			]
 		for page in pages:
 			self._profile_required("/" + page + "/")
-			
 
 	def test_anonymous_user(self):
-		self.client.login(username="su", password="password")
+		self.client.login(username="su", password="pwd")
 		response = self.client.get("/custom_admin/anonymous_login/", follow=True)
 		self.assertRedirects(response, "/")
 		response = self.client.get("/")
@@ -197,7 +201,7 @@ class ManagementPermission(TestCase):
 
 		# Test that we are "Anonymous Coward"?
 
-		self.client.login(username="su", password="password")
+		self.client.login(username="su", password="pwd")
 		response = self.client.get("/custom_admin/end_anonymous_session/", follow=True)
 		self.assertRedirects(response, "/custom_admin/utilities/")
 		self.assertIn(MESSAGES["ANONYMOUS_SESSION_ENDED"], response.content)
@@ -205,7 +209,7 @@ class ManagementPermission(TestCase):
 		self.client.logout()
 
 	def test_profile_request(self):
-		self.client.login(username="su", password="password")
+		self.client.login(username="su", password="pwd")
 
 		response = self.client.get("/custom_admin/profile_requests/{0}/"
 					   .format(self.pr.pk))
