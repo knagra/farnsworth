512dea6b9a98037914d7ea5f5d9ff33f771df171
Nader Morshed <morshed.nader@gmail.com>
2014-05-10 23:58:58 -0700

    Fixed admin_required

--- threads/decorators.py
+++ threads/decorators.py
@@ -7,6 +7,7 @@ Author: Karandeep Singh Nagra
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+from farnsworth.settings import MESSAGES
 from threads.models import UserProfile
 from threads.redirects import red_home
 
@@ -19,8 +20,7 @@ def profile_required(function=None, redirect_user='login', redirect_profile=red_
 				UserProfile.objects.get(user=request.user)
 			except UserProfile.DoesNotExist:
 				return redirect_profile(request, MESSAGES['NO_PROFILE'])
-			else:
-				return view_func(request, *args, **kwargs)
+			return view_func(request, *args, **kwargs)
 		return wrap
 	if function:
 		return real_decorator(function)
@@ -35,10 +35,9 @@ def admin_required(function=None, redirect_user='login', redirect_profile=red_ho
 				UserProfile.objects.get(user=request.user)
 			except UserProfile.DoesNotExist:
 				return redirect_profile(request, MESSAGES['NO_PROFILE'])
-			else:
-				return view_func(request, *args, **kwargs)
 			if not request.user.is_superuser:
 				return redirect_profile(request, MESSAGES['ADMINS_ONLY'])
+			return view_func(request, *args, **kwargs)
 		return wrap
 	if function:
 		return real_decorator(function)
