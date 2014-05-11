c292fe5bbc1e863a20dd573ec85f02897da5ce41
Nader Morshed <morshed.nader@gmail.com>
2014-05-10 21:38:58 -0700

    Added arguments to profile_exists

--- threads/decorators.py
+++ threads/decorators.py
@@ -5,19 +5,22 @@ Author: Karandeep Singh Nagra
 '''
 
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from threads.models import UserProfile
-from threads.views import red_home
+from threads.redirects import red_home
 
-def profile_exists(function):
-	def wrap(request, *args, **kwargs):
-		if request.user.is_authenticated():
+def profile_exists(redirect_user='login', redirect_profile=red_home):
+	def real_decorator(view_func):
+		def wrap(request, *args, **kwargs):
+			if not request.user.is_authenticated():
+				return HttpResponseRedirect(reverse(redirect_user))
 			try:
 				UserProfile.objects.get(user=request.user)
 			except UserProfile.DoesNotExist:
-				return red_home(request, MESSAGES['NO_PROFILE'])
-		else:
-			return HttpResponseRedirect(reverse('login'))
-		return function(request, *args, **kwargs)
-	return wrap
+				return redirect_profile(request, MESSAGES['NO_PROFILE'])
+			else:
+				return view_func(request, *args, **kwargs)
+		return wrap
+	return real_decorator
 
