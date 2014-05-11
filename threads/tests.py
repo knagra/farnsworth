c62715a690135619d087c09823411029227eda12
Nader Morshed <morshed.nader@gmail.com>
2014-05-10 23:18:15 -0700

    Added trivial tests for threads

--- threads/tests.py
+++ threads/tests.py
@@ -5,21 +5,38 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from datetime import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
-from models import UserProfile
+from threads.models import UserProfile, Thread
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_basic_addition(self):
+		"""
+		Tests that 1 + 1 always equals 2.
+		"""
+		self.assertEqual(1 + 1, 2)
 
 class VerifyUser(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username="user", email="fake@email.com", first_name="john", last_name="doe", password="password")
+		self.user.save()
+
 	def test_user_profile_created(self):
 		''' Test that the user profile for a user is automatically created when a user is created. '''
-		new_user = User.objects.create_user(username="user", email="fake@email.com", first_name="john", last_name="doe", password="password")
-		new_user.save()
-		self.assertNotEqual(0, UserProfile.objects.filter(user=new_user).count())
-		self.assertEqual(new_user, UserProfile.objects.get(user=new_user).user)
+		self.assertEqual(1, UserProfile.objects.filter(user=self.user).count())
+		self.assertEqual(self.user, UserProfile.objects.get(user=self.user).user)
+
+class VerifyThread(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username="user", email="fake@email.com", first_name="john", last_name="doe", password="password")
+		self.user.save()
+		now = datetime.now()
+		profile = UserProfile.objects.get(user=self.user)
+		self.thread = Thread(owner=profile, subject="subject")
+		self.thread.save()
+
+	def test_thread_created(self):
+		self.assertEqual(1, Thread.objects.all().count())
+		self.assertEqual(self.thread, Thread.objects.get(pk=self.thread.pk))
+		self.assertNotEqual(0, Thread.objects.filter(subject="subject").count())
