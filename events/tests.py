8c352e2d3325babb132b11a2e71a90b35c9f9429
Nader Morshed <morshed.nader@gmail.com>
2014-05-12 01:00:03 -0700

    Added a basic test that events can be viewed

--- events/tests.py
+++ events/tests.py
@@ -5,12 +5,36 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from datetime import datetime, timedelta
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils.timezone import utc
+from threads.models import UserProfile, Thread, Message
 
+from events.models import Event
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestEvent(TestCase):
+	def setUp(self):
+		self.u = User.objects.create_user(username="u", email="u@email.com", password="pwd")
+		self.u.save()
+
+		profile = UserProfile.objects.get(user=self.u)
+		now = datetime.utcnow().replace(tzinfo=utc)
+		one_day = timedelta(days=1)
+
+		self.ev = Event(owner=profile, title="Event Title Test",
+				start_time=now, end_time=now + one_day)
+		self.ev.save()
+
+		self.client.login(username="u", password="pwd")
+
+	def test_event_views(self):
+		urls = [
+			"/events/",
+			"/archives/all_events/",
+			]
+		for url in urls:
+			response = self.client.get(url)
+			self.assertEqual(response.status_code, 200)
+			self.assertIn(self.ev.title, response.content)
+
