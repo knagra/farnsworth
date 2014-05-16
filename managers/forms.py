abafc011dc6cb2065f52fd5d342353a503d233ab
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-05-15 17:43:10 -0700

    minor update to help page

--- managers/forms.py
+++ managers/forms.py
@@ -13,7 +13,7 @@ from utils.funcs import verify_username, verify_url
 
 class ProfileRequestForm(forms.Form):
 	''' Form to create a new profile request. '''
-	username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}), help_text='Characters A-Z, a-z, 0-9, or "_".')
+	username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}), help_text='Characters A-Z, a-z, 0-9, or _.')
 	first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}))
 	last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}))
 	email = forms.EmailField(max_length=100)
@@ -29,7 +29,7 @@ class ProfileRequestForm(forms.Form):
 		if not super(ProfileRequestForm, self).is_valid():
 			return False
 		elif not verify_username(self.cleaned_data['username']):
-			self._errors['username'] = self.error_class([u'Invalid username. Must be characters A-Z, a-z, 0-9, or "_"'])
+			self._errors['username'] = self.error_class([u'Invalid username. Must be characters A-Z, a-z, 0-9, or _.'])
 			return False
 		elif self.cleaned_data['password'] != self.cleaned_data['confirm_password']:
 			self._errors['password'] = forms.util.ErrorList([u"Passwords don't match."])
@@ -65,7 +65,7 @@ class AddUserForm(forms.Form):
 		if not super(AddUserForm, self).is_valid():
 			return False
 		elif not verify_username(self.cleaned_data['username']):
-			self._errors['username'] = self.error_class([u'Invalid username. Must be characters A-Z, a-z, 0-9, or "_"'])
+			self._errors['username'] = self.error_class([u'Invalid username. Must be characters A-Z, a-z, 0-9, or _.'])
 			return False
 		elif self.cleaned_data['user_password'] != self.cleaned_data['confirm_password']:
 			self._errors['user_password'] = forms.util.ErrorList([u"Passwords don't match."])
@@ -110,7 +110,7 @@ class ChangeUserPasswordForm(forms.Form):
 
 class ModifyProfileRequestForm(forms.Form):
 	''' Form to modify a profile request. '''
-	username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}), help_text='Characters A-Z, a-z, 0-9, or "_".')
+	username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}), help_text='Characters A-Z, a-z, 0-9, or _.')
 	first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}))
 	last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'50'}))
 	email = forms.EmailField(max_length=100, required=False)
@@ -134,7 +134,7 @@ class ModifyProfileRequestForm(forms.Form):
 		if not super(ModifyProfileRequestForm, self).is_valid():
 			return False
 		elif not verify_username(self.cleaned_data['username']):
-			self._errors['username'] = self.error_class([u'Invalid username. Must be characters A-Z, a-z, 0-9, or "_".'])
+			self._errors['username'] = self.error_class([u'Invalid username. Must be characters A-Z, a-z, 0-9, or _.'])
 			return False
 		return True
 
