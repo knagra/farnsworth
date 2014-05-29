4e9fb896fe79d1a648cb9e7b706b28162f3451de
Nader Morshed <morshed.nader@gmail.com>
2014-05-29 00:21:38 -0700

    Added very basic testing to the views

--- workshift/tests.py
+++ workshift/tests.py
@@ -1,7 +1,35 @@
+
 from django.test import TestCase
 
-from workshift.models import *
+from utils.funcs import convert_to_url
+from base.models import User, UserProfile
+from managers.models import Manager
+from workshift.models import Semester
 
-class TestModel(TestCase):
-    def test_models(self):
-        pass
+class BasicTest(TestCase):
+	def setUp(self):
+		self.su = User.objects.create_user(username="su", password="pwd")
+		self.su.is_staff, self.su.is_superuser = True, True
+		self.su.save()
+
+		self.m = Manager(
+			title="Workshift Manager",
+			incumbent=UserProfile.objects.get(user=self.su),
+			)
+		self.m.url_title = convert_to_url(self.m.title)
+		self.m.workshift_manager = True
+		self.m.save()
+
+		self.sem = Semester(year=2014)
+		self.sem.save()
+
+		self.assertTrue(self.client.login(username="su", password="pwd"))
+
+	def test_views(self):
+		urls = [
+			"/workshift/start/"
+			"/workshift/"
+			]
+		for url in urls:
+			response = self.client.get(url)
+			self.assertEqual(response.status_code, 200)
