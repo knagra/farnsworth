a86a659e9b2e13b956063bea56a329b059317a05
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-05-29 01:09:41 -0700

    fixed odd tabs and spaces in settings.py

---
+++ workshift/search_indexes.py
@@ -0,0 +1,42 @@
+'''
+Project: Farnsworth
+
+Author: Karandeep Singh Nagra
+
+Search indexes for the workshift app.
+'''
+
+from datetime import datetime
+from haystack import indexes
+
+from workshift.models import Semester, WorkshiftType, WorkshiftProfile, \
+	RegularWorkshift, WorkshiftInstance, OneTimeWorkshift
+
+class SemesterIndex(indexes.SearchIndex, indexes.Indexable):
+	''' Index for Semesters. '''
+	text = indexes.EdgeNgramField(document=True, use_template=True)
+	owner = indexes.EdgeNgramField(model_attr='owner')
+	exact_user = indexes.CharField(model_attr='owner', faceted=True)
+	subject = indexes.EdgeNgramField(model_attr='subject', boost=1.125)
+	start_date = indexes.DateTimeField(model_attr='start_date')
+	change_date = indexes.DateTimeField(model_attr='change_date')
+	
+	def get_model(self):
+		return Thread
+	
+	def index_queryset(self, using=None):
+		return self.get_model().objects.filter(active=True)
+
+class MessageIndex(indexes.SearchIndex, indexes.Indexable):
+	''' Index for Messages. '''
+	text = indexes.EdgeNgramField(document=True, use_template = True)
+	owner = indexes.EdgeNgramField(model_attr='owner')
+	exact_user = indexes.CharField(model_attr='owner', faceted=True)
+	body = indexes.EdgeNgramField(model_attr='body')
+	post_date = indexes.DateTimeField(model_attr='post_date')
+	
+	def get_model(self):
+		return Message
+	
+	def index_queryset(self, using=None):
+		return self.get_model().objects.filter(thread__active=True)
