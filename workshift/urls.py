f2d79a0752fd11ae74eb74b14f4b95ff94be1e3a
Nader Morshed <morshed.nader@gmail.com>
2014-05-28 21:51:06 -0700

    Updated the urls a bit

--- workshift/urls.py
+++ workshift/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from workshift import views
 
-urlpatterns = [
+urlpatterns = patterns("",
 	url(r"^workshift/$", views.main_view, name="main"),
 	url(r"^workshift/preferences/(P<semester>\w+)/$", views.preferences_view, name="preferences"),
 	url(r"^workshift/manage/$", views.manage_view, name="manage"),
@@ -11,4 +11,4 @@ urlpatterns = [
 	url(r"^workshift/manage/add_shift/$", views.add_shift_view, name="add_shift"),
 	url(r"^workshift/shifts/(P<shift_title>\w+)/$", views.shift_view, name="view_shift"),
 	url(r"^workshift/shifts/(P<shift_title>\w+)/edit$", views.edit_shift_view, name="edit_shift"),
-]
+)
