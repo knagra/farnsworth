55b22f7097b64830abcf4d15c638fe14fdf4acd1
Nader Morshed <morshed.nader@gmail.com>
2014-05-29 02:25:48 -0700

    Include shift log entries in the tests

--- workshift/tests.py
+++ workshift/tests.py
@@ -8,7 +8,8 @@ from utils.funcs import convert_to_url
 from base.models import User, UserProfile
 from managers.models import Manager
 from workshift.models import Semester, WorkshiftPool, WorkshiftProfile, \
-	 WorkshiftType, RegularWorkshift, WorkshiftInstance, OneTimeWorkshift
+	 WorkshiftType, RegularWorkshift, WorkshiftInstance, OneTimeWorkshift, \
+	 ShiftLogEntry
 
 class BasicTest(TestCase):
 	def setUp(self):
@@ -66,6 +67,41 @@ class BasicTest(TestCase):
 			date=date.today(),
 			workshifter=self.wprofile,
 			)
+		self.once.save()
+
+		self.sle0 = ShiftLogEntry(
+			person=self.wprofile,
+			note="Test Shift Log #0",
+			entry_type=ShiftLogEntry.ASSIGNED,
+			)
+
+		self.sle1 = ShiftLogEntry(
+			person=self.wprofile,
+			note="Test Shift Log #1",
+			entry_type=ShiftLogEntry.SIGNOUT,
+			)
+
+		self.sle2 = ShiftLogEntry(
+			person=self.wprofile,
+			note="Test Shift Log #2",
+			entry_type=ShiftLogEntry.SIGNIN,
+			)
+
+		self.sle3 = ShiftLogEntry(
+			person=self.wprofile,
+			note="Test Shift Log #3",
+			entry_type=ShiftLogEntry.VERIFY,
+			)
+
+		self.sle0.save()
+		self.sle1.save()
+		self.sle2.save()
+		self.sle3.save()
+
+		self.instance.shift_log = [self.sle0, self.sle1, self.sle2, self.sle3]
+		self.once.shift_log = [self.sle0, self.sle1, self.sle2, self.sle3]
+
+		self.instance.save()
 		self.once.save()
 
 		self.assertTrue(self.client.login(username="su", password="pwd"))
@@ -141,6 +177,7 @@ class BasicTest(TestCase):
 		self.assertIn(self.shift.hours, response.content)
 		self.assertIn(self.shift.quick_tips, response.content)
 		self.assertIn(self.shift.description, response.content)
+		self.assertIn(self.shift.current_assignee.user.get_full_name(), response.content)
 
 		response = self.client.get("/workshift/shift/{0}/edit/".format(self.shift.pk))
 		self.assertEqual(response.status_code, 200)
@@ -148,6 +185,7 @@ class BasicTest(TestCase):
 		self.assertIn(self.shift.hours, response.content)
 		self.assertIn(self.shift.quick_tips, response.content)
 		self.assertIn(self.shift.description, response.content)
+		self.assertIn(self.shift.current_assignee.user.get_full_name(), response.content)
 
 	def test_instance(self):
 		response = self.client.get("/workshift/instance/{0}/".format(self.instance.pk))
@@ -157,6 +195,10 @@ class BasicTest(TestCase):
 		self.assertIn(self.instance.date, response.content)
 		self.assertIn(self.instance.workshifter.user.get_full_name(), response.content)
 		self.assertIn(self.instance.hours, response.content)
+		self.assertIn(self.sle0.note, response.content)
+		self.assertIn(self.sle1.note, response.content)
+		self.assertIn(self.sle2.note, response.content)
+		self.assertIn(self.sle3.note, response.content)
 
 		response = self.client.get("/workshift/instance/{0}/edit/".format(self.instance.pk))
 		self.assertEqual(response.status_code, 200)
