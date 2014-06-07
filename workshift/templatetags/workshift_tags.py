d786cd85f4bedb76c5301eccf10d579af69068a3
Nader Morshed <morshed.nader@gmail.com>
2014-06-06 23:59:20 -0700

    Fixed syntax error

--- workshift/templatetags/workshift_tags.py
+++ workshift/templatetags/workshift_tags.py
@@ -10,7 +10,7 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 @register.filter
-def wurl(url_name, *args, **kwargs)
-    args = [i for i in args if i]
-    kwargs = dict((i, j) for i, j in kwargs.items() if j)
-    return reverse(url_name, args=args, kwargs=kwargs)
+def wurl(url_name, *args, **kwargs):
+	args = [i for i in args if i]
+	kwargs = dict((i, j) for i, j in kwargs.items() if j)
+	return reverse(url_name, args=args, kwargs=kwargs)
