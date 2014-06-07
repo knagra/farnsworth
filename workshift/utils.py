f77ec2d170d0b024fdedacdeaf0fb3950ee080f9
Nader Morshed <morshed.nader@gmail.com>
2014-06-06 22:27:10 -0700

    Fixed a bug when semester=None

--- workshift/utils.py
+++ workshift/utils.py
@@ -10,11 +10,9 @@ def can_manage(request, semester=None):
 	current workshift managers, that semester's workshift managers, and site
 	superusers.
 	"""
-	if semester:
-		semester_managers = semeter.workshift_managers.all()
-	if Manager:
-		workshift_managers = Manager.objects.filter(incumbent__user=request.user) \
-		  .filter(workshift_manager=True)
-	return request.user in semester_managers or \
-	  workshift_managers.count() or \
-	  request.user.is_superuser
+	if semester and request.user in semester.workshift_managers.all():
+		return True
+	if Manager and Manager.objects.filter(incumbent__user=request.user) \
+	  .filter(workshift_manager=True).count() > 0:
+		return True
+	return request.user.is_superuser
