8a00c491b4641202d9dbac666ef8f0ac59bab73f
Nader Morshed <morshed.nader@gmail.com>
2014-06-26 16:57:02 -0700

    Fixed reading the README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
-README = open(os.path.join(os.path.dirname(__file__), "README.txt")).read()
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 required = [
