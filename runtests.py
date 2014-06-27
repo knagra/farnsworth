73e4eca08468ab6a9165fd248666cb4abe565438
Nader Morshed <morshed.nader@gmail.com>
2014-06-26 19:40:30 -0700

    Fixed two more failing tests, added rooms to runtests.py

--- runtests.py
+++ runtests.py
@@ -18,7 +18,13 @@ def runtests():
 
 	TestRunner = get_runner(settings)
 	test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
-	failures = test_runner.run_tests(["base", "threads", "events", "managers"])
+	failures = test_runner.run_tests([
+		"base",
+		"threads",
+		"events",
+		"managers",
+		"rooms",
+		])
 	sys.exit(bool(failures))
 
 if __name__ == "__main__":
