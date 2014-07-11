59c1c6b404358acd5cb122bcae89418a128da0bf
Nader Morshed <morshed.nader@gmail.com>
2014-07-11 12:53:13 -0700

    Bumped the version for when we eventually release dev

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ required = [
 
 setup(
     name="Farnsworth",
-    version="1.2.1",
+    version="2.0.0",
     author="Karandeep Nagra",
     url="https://github.com/knagra/farnsworth",
     author_email="karandeepsnagra@gmail.com",
