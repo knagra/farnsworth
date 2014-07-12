cda33252c309d54fdcc28bbcfa2027704d37bf2d
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-07-11 17:39:32 -0700

    minor update

--- threads/forms.py
+++ threads/forms.py
@@ -13,53 +13,58 @@ from base.models import UserProfile
 from threads.models import Thread, Message
 
 class ThreadForm(forms.Form):
-	''' Form to post a new thread. '''
-	subject = forms.CharField(max_length=300, widget=forms.TextInput(attrs={'size':'100'}), required=True)
-	body = forms.CharField(widget=forms.Textarea())
+    ''' Form to post a new thread. '''
+    subject = forms.CharField(max_length=300, widget=forms.TextInput(attrs={'size':'100'}), required=True)
+    body = forms.CharField(widget=forms.Textarea())
 
-	def __init__(self, *args, **kwargs):
-		self.profile = kwargs.pop('profile')
-		super(ThreadForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.profile = kwargs.pop('profile')
+        super(ThreadForm, self).__init__(*args, **kwargs)
 
-	def save(self):
-	   thread = Thread(
-		   owner=self.profile,
-		   subject=self.cleaned_data['subject'],
-		   number_of_messages=1,
-		   active=True,
-		   )
-	   thread.save()
-	   message = Message(
-		   body=self.cleaned_data['body'],
-		   owner=self.profile,
-		   thread=thread,
-		   )
-	   message.save()
+    def save(self):
+       thread = Thread(
+           owner=self.profile,
+           subject=self.cleaned_data['subject'],
+           number_of_messages=1,
+           active=True,
+           )
+       thread.save()
+       message = Message(
+           body=self.cleaned_data['body'],
+           owner=self.profile,
+           thread=thread,
+           )
+       message.save()
 
 class MessageForm(forms.Form):
-	''' Form to post a new message. '''
-	thread_pk = forms.IntegerField(widget=forms.HiddenInput())
-	body = forms.CharField(widget=forms.Textarea())
+    ''' Form to post a new message. '''
+    thread_pk = forms.IntegerField(widget=forms.HiddenInput())
+    body = forms.CharField(widget=forms.Textarea())
 
-	def __init__(self, *args, **kwargs):
-		self.profile = kwargs.pop('profile')
-		super(MessageForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.profile = kwargs.pop('profile')
+        super(MessageForm, self).__init__(*args, **kwargs)
 
-	def clean_thread_pk(self):
-		try:
-			thread = Thread.objects.get(pk=self.cleaned_data["thread_pk"])
-		except Thread.DoesNotExist:
-			raise ValidationError("Thread does not exist.")
-		return thread
+    def clean_thread_pk(self):
+        try:
+            thread = Thread.objects.get(pk=self.cleaned_data["thread_pk"])
+        except Thread.DoesNotExist:
+            raise ValidationError("Thread does not exist.")
+        return thread
 
-	def save(self):
-		thread = self.cleaned_data["thread_pk"]
-		message = Message(
-			body=self.cleaned_data["body"],
-			owner=self.profile,
-			thread=thread,
-			)
-		message.save()
-		thread.number_of_messages += 1
-		thread.change_date = datetime.utcnow().replace(tzinfo=utc)
-		thread.save()
+    def save(self):
+        thread = self.cleaned_data["thread_pk"]
+        message = Message(
+            body=self.cleaned_data["body"],
+            owner=self.profile,
+            thread=thread,
+            )
+        message.save()
+        thread.number_of_messages += 1
+        thread.change_date = datetime.utcnow().replace(tzinfo=utc)
+        thread.save()
+
+    def update(message, self):
+        thread = self.cleaned_data["thread_pk"]
+        message.body = self.cleaned_data["body"]
+        message.save()
--- threads/models.py
+++ threads/models.py
@@ -10,39 +10,39 @@ from django.contrib.auth.models import User, Group, Permission
 from base.models import UserProfile
 
 class Thread(models.Model):
-	'''
-	The Thread model.  Used to group messages.
-	'''
-	owner = models.ForeignKey(UserProfile, help_text="The user who started this thread.")
-	subject = models.CharField(blank=False, null=False, max_length=254, help_text="Subject of this thread.")
-	start_date = models.DateTimeField(auto_now_add=True, help_text="The date this thread was started.")
-	change_date = models.DateTimeField(auto_now_add=True, help_text="The last time this thread was modified.")
-	number_of_messages = models.PositiveSmallIntegerField(default=1, help_text="The number of messages in this thread.")
-	active = models.BooleanField(default=True, help_text="Whether this thread is still active.")
+    '''
+    The Thread model.  Used to group messages.
+    '''
+    owner = models.ForeignKey(UserProfile, help_text="The user who started this thread.")
+    subject = models.CharField(blank=False, null=False, max_length=254, help_text="Subject of this thread.")
+    start_date = models.DateTimeField(auto_now_add=True, help_text="The date this thread was started.")
+    change_date = models.DateTimeField(auto_now_add=True, help_text="The last time this thread was modified.")
+    number_of_messages = models.PositiveSmallIntegerField(default=1, help_text="The number of messages in this thread.")
+    active = models.BooleanField(default=True, help_text="Whether this thread is still active.")
 
-	def __unicode__(self):
-		return self.subject
+    def __unicode__(self):
+        return self.subject
 
-	class Meta:
-		ordering = ['-change_date']
+    class Meta:
+        ordering = ['-change_date']
 
-	def is_thread(self):
-		return True
+    def is_thread(self):
+        return True
 
 class Message(models.Model):
-	'''
-	The Message model.  Contains a body, owner, and post_date, referenced by thread.
-	'''
-	body = models.TextField(blank=False, null=False, help_text="Body of this message.")
-	owner = models.ForeignKey(UserProfile, help_text="The user who posted this message.")
-	post_date = models.DateTimeField(auto_now_add=True, help_text="The date this message was posted.")
-	thread = models.ForeignKey(Thread, help_text="The thread to which this message belongs.")
+    '''
+    The Message model.  Contains a body, owner, and post_date, referenced by thread.
+    '''
+    body = models.TextField(blank=False, null=False, help_text="Body of this message.")
+    owner = models.ForeignKey(UserProfile, help_text="The user who posted this message.")
+    post_date = models.DateTimeField(auto_now_add=True, help_text="The date this message was posted.")
+    thread = models.ForeignKey(Thread, help_text="The thread to which this message belongs.")
 
-	def __unicode__(self):
-		return "Message by %s on thread %s, posted %s" % (self.owner, self.thread.subject, self.post_date)
+    def __unicode__(self):
+        return "Message by %s on thread %s, posted %s" % (self.owner, self.thread.subject, self.post_date)
 
-	class Meta:
-		ordering = ['post_date']
+    class Meta:
+        ordering = ['post_date']
 
-	def is_message(self):
-		return True
+    def is_message(self):
+        return True
