0b6ccce654939ec7ad77f1f674b707e305667c91
Nader Morshed <morshed.nader@gmail.com>
2014-07-14 19:11:22 -0700

    Use setdefault

--- events/forms.py
+++ events/forms.py
@@ -53,25 +53,24 @@ class EventForm(forms.Form):
         self.manager_positions = Manager.objects.filter(incumbent=self.profile)
         if 'instance' in kwargs:
             self.instance = kwargs.pop('instance')
-            if 'initial' not in kwargs:
-                rsvpd = (self.profile in self.instance.rsvps.all())
-                kwargs['initial'] = {
+            kwargs.setdefault(
+                "initial",
+                {
                     'title': self.instance.title,
                     'description': self.instance.description,
                     'location': self.instance.location,
-                    'rsvp': rsvpd,
+                    'rsvp': self.profile in self.instance.rsvps.all(),
                     'start_time': self.instance.start_time,
                     'end_time': self.instance.end_time,
                     'as_manager': self.instance.as_manager,
-                    'cancelled': self.instance.cancelled
-                    }
+                    'cancelled': self.instance.cancelled,
+                })
         else:
             self.instance = None
-        if 'initial' not in kwargs:
-            kwargs['initial'] = {
-                "rsvp": True,
-                "location": settings.HOUSE,
-                }
+        kwargs.setdefault(
+            "initial",
+            {"rsvp": True, "location": settings.HOUSE},
+            )
         super(EventForm, self).__init__(*args, **kwargs)
         if self.manager_positions:
             self.fields['as_manager'].queryset = self.manager_positions
