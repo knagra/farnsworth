dfbc02b7922e807fa14097d93142e2b250304bac
Nader Morshed <morshed.nader@gmail.com>
2014-07-16 08:42:17 -0700

    Django 1.7 compatibility changes

--- runtests.py
+++ runtests.py
@@ -8,10 +8,15 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 if this_dir not in sys.path:
     sys.path.insert(0, this_dir)
 
+import django
 from django.test.utils import get_runner
 from django.conf import settings
 
 def runtests():
+    # Compatibility with Django 1.7's stricter initialization
+    if hasattr(django, "setup"):
+        django.setup()
+
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
     failures = test_runner.run_tests([
