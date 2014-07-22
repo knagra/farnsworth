5a98db53c2f072b2f6fc08d35f330f599d713f75
Karandeep Singh Nagra <knagra@users.noreply.github.com>
2014-07-22 09:39:18 -0700

    Update search_indexes.py

--- base/search_indexes.py
+++ base/search_indexes.py
@@ -14,7 +14,7 @@ from utils.variables import ANONYMOUS_USERNAME
 class UserProfileIndex(indexes.SearchIndex, indexes.Indexable):
     ''' Index for UserProfiles. '''
     text = indexes.EdgeNgramField(document=True, use_template=True)
-    user = indexes.EdgeNgramField(model_attr='user', boost=2.5)
+    user = indexes.EdgeNgramField(model_attr='user', boost=1.125)
     exact_user = indexes.CharField(model_attr='user', faceted=True)
     former_houses = indexes.EdgeNgramField(model_attr='former_houses', null=True)
     status = indexes.EdgeNgramField(model_attr='status')
@@ -22,6 +22,11 @@ class UserProfileIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return UserProfile
+
+    def prepare(self, obj):
+        data = super(UserProfileIndex, self).prepare(obj)
+        data['boost'] = 1.125
+        return data
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all().exclude(user__username=ANONYMOUS_USERNAME)
