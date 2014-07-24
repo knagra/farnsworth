1bc51b951816db37d2bec764c3490b1faad412bc
Karandeep Singh Nagra <knagra@users.noreply.github.com>
2014-07-24 13:06:20 -0700

    Update forms.py

--- managers/forms.py
+++ managers/forms.py
@@ -130,13 +130,13 @@ class ManagerResponseForm(ResponseForm):
     def __init__(self, *args, **kwargs):
         super(ManagerResponseForm, self).__init__(*args, **kwargs)
         if self.request.closed:
-            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Request.CLOSED)
+            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Response.CLOSED)
         if self.request.open:
-            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Request.OPEN)
+            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Response.REOPENED)
         if self.request.filled:
-            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Request.FILLED)
+            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Response.FILLED)
         if self.request.expired:
-            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Request.EXPIRED)
+            self.fields['action'].choices = (choice for choice in Response.ACTION_CHOICES if choice != Response.EXPIRED)
 
     def save(self):
         response = super(ManagerResponseForm, self).save()
