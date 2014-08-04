c137b025c33c46589bf85b01f9b9e019fb30be34
Karandeep Singh Nagra <knagra@users.noreply.github.com>
2014-08-04 12:03:55 -0700

    bugfix, improvement, and documentation

--- managers/cron.py
+++ managers/cron.py
@@ -11,10 +11,19 @@ from django.utils.timezone import now
 from django_cron import CronJobBase, Schedule
 
 from farnsworth.settings import REQUEST_EXPIRATION_HOURS
-from managers.models import Request
+from managers.models import Request, Response
 
 
 class ExpireRequests(CronJobBase):
+    """
+    Expire OPEN requests that are more than REQUEST_EXPIRATION_HOURS old
+    and have no REOPENED responses and no responses within the past
+    REQUEST_EXPIRATION_HOURS associated with them.
+    REOPENED responses are omitted to allow managers to reopen expired requests
+    and not have those requests automatically expired by the system again.
+    Responses within the past REQUEST_EXPIRATION_HOURS exclude expiration
+    as such responses demontrate ongoing value or interest.
+    """
     RUN_AT_TIMES = ['00:01',]
     RUN_EVERY_MINS = 60
 
@@ -22,6 +31,11 @@ class ExpireRequests(CronJobBase):
     code = 'managers.expire_requests'
 
     def do(self):
-        for req in Request.objects.filter(post_date__lte=now()-timedelta(hours=REQUEST_EXPIRATION_HOURS)):
-            req.status = Request.EXPIRED
-            req.save()
+        for req in Request.objects.filter(
+          status=Request.OPEN,
+          post_date__lte=now()-timedelta(hours=REQUEST_EXPIRATION_HOURS),
+          ):
+            if not req.response_set.filter(action=Response.REOPENED).count() and \
+              not req.response_set.filter(post_date__lte=now()-timedelta(hours=REQUEST_EXPIRATION_HOURS)).count():
+                req.status = Request.EXPIRED
+                req.save()
