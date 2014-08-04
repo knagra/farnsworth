ec82d576706b933e482eec39f96626dba59cce9a
Karandeep Singh Nagra <knagra@users.noreply.github.com>
2014-08-04 11:42:17 -0700

    auto-expiring old requests

---
+++ managers/cron.py
@@ -0,0 +1,27 @@
+"""
+Project: Farnsworth
+
+Authors: Karandeep Singh Nagra and Nader Morshed
+"""
+
+from datetime import timedelta
+
+from django.utils.timezone import now
+
+from django_cron import CronJobBase, Schedule
+
+from farnsworth.settings import REQUEST_EXPIRATION_HOURS
+from managers.models import Request
+
+
+class ExpireRequests(CronJobBase):
+    RUN_AT_TIMES = ['00:01',]
+    RUN_EVERY_MINS = 15
+
+    schedule = Schedule(run_at_times=RUN_AT_TIMES, run_every_mins=RUN_EVERY_MINS)
+    code = 'managers.expire_requests'
+
+    def do(self):
+        for req in Request.objects.filter(post_date__lte=now()-timedelta(hours=REQUEST_EXPIRATION_HOURS)):
+            req.status = Request.EXPIRED
+            req.save()
