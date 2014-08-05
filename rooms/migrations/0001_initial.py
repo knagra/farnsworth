711f31ec623a1f3b7840ddea66ad35e35590765c
Nader Morshed <morshed.nader@gmail.com>
2014-08-04 23:40:17 -0700

    More py3 fixes

--- rooms/migrations/0001_initial.py
+++ rooms/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('start_date', models.DateField(help_text=b"Start date of this person's residence in this room.")),
                 ('end_date', models.DateField(help_text=b"End date of this person's residence in this room.")),
-                ('resident', models.ForeignKey(help_text=b'The resident.', to='base.UserProfile')),
+                ('resident', models.ForeignKey(help_text='The resident.', to='base.UserProfile')),
             ],
             options={
             },
@@ -28,10 +28,10 @@ class Migration(migrations.Migration):
             name='Room',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(help_text=b"The title of the room (e.g., '2E'). Characters A-Z, 0-9.", unique=True, max_length=100, validators=[django.core.validators.RegexValidator(b'^[0-9A-Za-z]+$', b'Only alphanumeric characters are allowed.')])),
+                ('title', models.CharField(help_text=b"The title of the room (e.g., '2E'). Characters A-Z, 0-9.", unique=True, max_length=100, validators=[django.core.validators.RegexValidator('^[0-9A-Za-z]+$', 'Only alphanumeric characters are allowed.')])),
                 ('unofficial_name', models.CharField(help_text=b"The unofficial name of the room (e.g., 'Starry Night')", max_length=100, null=True, blank=True)),
-                ('description', models.TextField(help_text=b'The description of this room.', null=True, blank=True)),
-                ('occupancy', models.IntegerField(default=1, help_text=b'The total number of people that this room should house.', validators=[django.core.validators.MinValueValidator(0)])),
+                ('description', models.TextField(help_text='The description of this room.', null=True, blank=True)),
+                ('occupancy', models.IntegerField(default=1, help_text='The total number of people that this room should house.', validators=[django.core.validators.MinValueValidator(0)])),
                 ('current_residents', models.ManyToManyField(to='base.UserProfile', blank=True)),
             ],
             options={
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='previousresident',
             name='room',
-            field=models.ForeignKey(help_text=b'The relevant room.', to='rooms.Room'),
+            field=models.ForeignKey(help_text='The relevant room.', to='rooms.Room'),
             preserve_default=True,
         ),
     ]
--- threads/migrations/0002_auto_20140801_1108.py
+++ threads/migrations/0002_auto_20140801_1108.py
@@ -21,12 +21,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='thread',
             name='views',
-            field=models.PositiveIntegerField(default=0, help_text=b'The number times this thread has been viewed.'),
+            field=models.PositiveIntegerField(default=0, help_text='The number times this thread has been viewed.'),
             preserve_default=True,
         ),
         migrations.AlterField(
             model_name='thread',
             name='change_date',
-            field=models.DateTimeField(help_text=b'The last time this thread was modified.', auto_now=True),
+            field=models.DateTimeField(help_text='The last time this thread was modified.', auto_now=True),
         ),
     ]
