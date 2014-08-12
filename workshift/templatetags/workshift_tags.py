15ee1592ce3d77ed9a049c7c8b98087c60991b82
Nader Morshed <morshed.nader@gmail.com>
2014-08-12 14:01:27 -0700

    Fixed a typo

--- workshift/templatetags/workshift_tags.py
+++ workshift/templatetags/workshift_tags.py
@@ -19,7 +19,7 @@ def wurl(url_name, *args, **kwargs):
 @register.filter
 def currency(value):
     dollars = round(float(value), 2)
-    minus = "-" if dollar < 0 else ""
+    minus = "-" if dollars < 0 else ""
     return "{0}${1}{2}".format(
         minus,
         intcomma(int(dollars)),
