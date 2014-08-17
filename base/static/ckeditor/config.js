f8d2a29094d7b271a322eacb99845bb58787bd39
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-08-16 20:21:42 -0700

    most of the way through integrating ckeditor

--- base/static/ckeditor/config.js
+++ base/static/ckeditor/config.js
@@ -1,10 +1,9 @@
-/**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+/*
+ * Project: Farnsworth
+ * Authors: Karandeep Singh Nagra and Nader Morshed
  */
-
+ 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    config.extraPlugins = 'widget,lineutils,pbckcode,mathjax,leaflet,autolink,codemirror,insertpre,footnotes,oembed,imagebrowser';
+    config.skin = 'bootstrapck';
 };
