7182f843871f6d17d4442d4b77fa0e31d68a9a8b
Nader Morshed <morshed.nader@gmail.com>
2014-08-16 21:49:49 -0700

    Removed edit / delete permissions from anonymous user

--- farnswiki/hooks.py
+++ farnswiki/hooks.py
@@ -1,8 +1,13 @@
+
 from wiki.hooks import WikiDefaultHookset
+
+from utils.variables import ANONYMOUS_USERNAME
 
 class ProjectWikiHookset(WikiDefaultHookset):
     def _perm_check(self, wiki, user):
-        return user.is_authenticated() and user.is_active
+        return user.is_authenticated() and \
+          user.is_active and \
+          user.username != ANONYMOUS_USERNAME
 
     def can_create_page(self, wiki, user):
         return self._perm_check(wiki, user)
@@ -11,7 +16,8 @@ class ProjectWikiHookset(WikiDefaultHookset):
         return self._perm_check(page.wiki, user)
 
     def can_delete_page(self, page, user):
-        return self._perm_check(page.wiki, user) and user.is_staff or user.is_superuser
+        return self._perm_check(page.wiki, user) and \
+          (user.is_staff or user.is_superuser)
 
     def can_view_page(self, page, user):
         return True
