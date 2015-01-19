2a79897f9e851edc959b9dc1b6ea25ea3470d224
Nader Morshed <morshed.nader@gmail.com>
2015-01-19 17:19:30 -0500

    Fixed failing tests

--- managers/tests.py
+++ managers/tests.py
@@ -790,7 +790,8 @@ class TestAnnouncements(TestCase):
 
 class TestPreFill(TestCase):
     def test_pre_fill(self):
-        from farnsworth.pre_fill import main, REQUESTS, MANAGERS
+        from farnsworth.pre_fill import main
+        from managers.fill import REQUESTS, MANAGERS
         main(["--requests", "--managers"])
         for title in [i[0] for i in MANAGERS]:
             self.assertEqual(1, Manager.objects.filter(title=title).count())
