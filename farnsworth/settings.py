e92e2fb9394028479de258033209617706112eef
Nader Morshed <morshed.nader@gmail.com>
2015-01-21 17:27:49 -0500

    Be consistent about using quotes

--- farnsworth/settings.py
+++ farnsworth/settings.py
@@ -1,10 +1,10 @@
 # Django settings for farnsworth project.
 
-'''
+"""
 Project: Farnsworth
 
 Author: Karandeep Singh Nagra
-'''
+"""
 
 import os.path
 import sys
@@ -51,7 +51,7 @@ if NETWORK_MANAGER_PASSWORD:
     # E-mail settings.  Enter your username and password to use Gmail.
     # You can also use a different SMTP server by changing these settings.
     EMAIL_USE_TLS = True
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = "smtp.gmail.com"
     EMAIL_HOST_USER = HOUSE_ABBREV + "nm@bsc.coop"
     EMAIL_HOST_PASSWORD = NETWORK_MANAGER_PASSWORD
     EMAIL_PORT = 587
@@ -61,8 +61,8 @@ else:
 # E-mail blacklist.  E-mails will not be sent to these addresses no matter what.
 # Add e-mails here as strings separated by commas, to prevent the site from sending e-mails
 # to select addresses.  Here's an example:
-# EXAMPLE_EMAIL_BLACKLIST = ('karandeepsnagra@gmail.com',
-#    'some_user@some_domain.com',
+# EXAMPLE_EMAIL_BLACKLIST = ("karandeepsnagra@gmail.com",
+#    "some_user@some_domain.com",
 #    )
 EMAIL_BLACKLIST = ()
 
@@ -81,7 +81,7 @@ except NameError:
 
 if ENABLE_OAUTH:
     TEMPLATE_CONTEXT_PROCESSORS += (
-        'social.apps.django_app.context_processors.backends',
+        "social.apps.django_app.context_processors.backends",
     )
 
 try:
@@ -92,25 +92,25 @@ except NameError:
 if POSTGRES_PASSWORD:
     # PostgreSQL setup
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': SHORT_HOUSE_NAME.lower(),
-            'USER': SHORT_HOUSE_NAME.lower() + '_admin',
-            'PASSWORD': POSTGRES_PASSWORD,
-            'HOST': 'localhost',
-            'PORT': '',
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": SHORT_HOUSE_NAME.lower(),
+            "USER": SHORT_HOUSE_NAME.lower() + "_admin",
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": "localhost",
+            "PORT": "",
         },
     }
 else:
     # SQLite3 setup
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(os.path.dirname(__file__), 'farnsworth.db').replace('\\', '/'),
-            'USER': '',
-            'PASSWORD': '',
-            'HOST': '',
-            'PORT': '',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(os.path.dirname(__file__), "farnsworth.db").replace("\\", "/"),
+            "USER": "",
+            "PASSWORD": "",
+            "HOST": "",
+            "PORT": "",
         },
     }
 
@@ -131,11 +131,11 @@ else:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = "America/Los_Angeles"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -152,22 +152,22 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # In this case, the directory
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'media/').replace('\\', '/')
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "media/").replace("\\", "/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = BASE_URL + '/media/'
+MEDIA_URL = BASE_URL + "/media/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static/').replace('\\', '/')
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static/").replace("\\", "/")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = BASE_URL + '/static/'
+STATIC_URL = BASE_URL + "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -179,64 +179,64 @@ STATICFILES_DIRS = (
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#    "django.contrib.staticfiles.finders.DefaultStorageFinder",
 )
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#    'django.template.loaders.eggs.Loader',
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+#    "django.template.loaders.eggs.Loader",
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social.apps.django_app.middleware.SocialAuthExceptionMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "social.apps.django_app.middleware.SocialAuthExceptionMiddleware",
 )
 
-ROOT_URLCONF = 'farnsworth.urls'
+ROOT_URLCONF = "farnsworth.urls"
 
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'farnsworth.wsgi.application'
+# Python dotted path to the WSGI application used by Django"s runserver.
+WSGI_APPLICATION = "farnsworth.wsgi.application"
 
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates').replace('\\', '/'),
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates").replace("\\", "/"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'base',
-    'threads',
-    'events',
-    'managers',
-    'workshift',
-    'rooms',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "base",
+    "threads",
+    "events",
+    "managers",
+    "workshift",
+    "rooms",
     "django_cron",
     # "elections",
     # "legacy",
     "wiki",
     "farnswiki",
     "notifications",
-    'bootstrapform',
-    'haystack',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'social.apps.django_app.default',
+    "bootstrapform",
+    "haystack",
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "social.apps.django_app.default",
 )
 
 CRON_CLASSES = [
@@ -249,7 +249,7 @@ WIKI_HOOKSET = "farnswiki.hooks.ProjectWikiHookset"
 WIKI_PARSE = "farnswiki.hooks.parse"
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
+    "django.contrib.auth.backends.ModelBackend",
 )
 
 try:
@@ -260,25 +260,25 @@ except NameError:
 if ENABLE_OAUTH:
     try:
         if SOCIAL_AUTH_FACEBOOK_KEY and SOCIAL_AUTH_FACEBOOK_SECRET:
-            AUTHENTICATION_BACKENDS = ('social.backends.facebook.FacebookOAuth2',) + AUTHENTICATION_BACKENDS
+            AUTHENTICATION_BACKENDS = ("social.backends.facebook.FacebookOAuth2",) + AUTHENTICATION_BACKENDS
     except NameError:
         pass
 
     try:
         if SOCIAL_AUTH_GITHUB_KEY and SOCIAL_AUTH_GITHUB_SECRET:
-            AUTHENTICATION_BACKENDS = ('social.backends.github.GithubOAuth2',) + AUTHENTICATION_BACKENDS
+            AUTHENTICATION_BACKENDS = ("social.backends.github.GithubOAuth2",) + AUTHENTICATION_BACKENDS
     except NameError:
         pass
 
     try:
         if SOCIAL_AUTH_GOOGLE_OAUTH2_KEY and SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET:
-            AUTHENTICATION_BACKENDS = ('social.backends.google.GoogleOAuth2',) + AUTHENTICATION_BACKENDS
+            AUTHENTICATION_BACKENDS = ("social.backends.google.GoogleOAuth2",) + AUTHENTICATION_BACKENDS
     except NameError:
         pass
 
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'public_profile']
-SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_SCOPE = ["email", "public_profile"]
+SOCIAL_AUTH_GITHUB_SCOPE = ["user:email"]
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ["email"]
 
 LOGIN_URL = BASE_URL + "/login/"
 LOGOUT_URL = BASE_URL + "/logout/"
@@ -288,24 +288,24 @@ LOGIN_ERROR_URL = BASE_URL
 APPEND_SLASH = True
 
 SOCIAL_AUTH_PIPELINE = (
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.auth_allowed',
-    'social.pipeline.social_auth.social_user',
-    'social.pipeline.user.get_username',
-    'base.pipeline.request_user',
+    "social.pipeline.social_auth.social_details",
+    "social.pipeline.social_auth.social_uid",
+    "social.pipeline.social_auth.auth_allowed",
+    "social.pipeline.social_auth.social_user",
+    "social.pipeline.user.get_username",
+    "base.pipeline.request_user",
 )
 
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+SESSION_SERIALIZER = "django.contrib.sessions.serializers.JSONSerializer"
 
 PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.BCryptPasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.SHA1PasswordHasher",
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+    "django.contrib.auth.hashers.CryptPasswordHasher",
     )
 
 # A sample logging configuration. The only tangible logging
@@ -314,55 +314,55 @@ PASSWORD_HASHERS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse"
         }
     },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler"
         },
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
         },
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
         },
-        'elasticsearch': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        "elasticsearch": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
         },
-        'django.security.DisallowedHost': {
-            'handlers': ['null'],
-            'propagate': False,
+        "django.security.DisallowedHost": {
+            "handlers": ["null"],
+            "propagate": False,
         },
     },
 }
 
 # Haystack search backend setting.
 HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://localhost:9200/',
-        'INDEX_NAME': SHORT_HOUSE_NAME.lower(),
+    "default": {
+        "ENGINE": "haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine",
+        "URL": "http://localhost:9200/",
+        "INDEX_NAME": SHORT_HOUSE_NAME.lower(),
     },
 }
 
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+HAYSTACK_SIGNAL_PROCESSOR = "haystack.signals.RealtimeSignalProcessor"
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 50
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 ### Threads Settings
 # Max number of threads loaded in member_forums.
@@ -428,13 +428,13 @@ try:
 except ImportError:
     pass
 
-if 'test' in sys.argv:
+if "test" in sys.argv:
     PASSWORD_HASHERS = (
-        'django.contrib.auth.hashers.MD5PasswordHasher',
+        "django.contrib.auth.hashers.MD5PasswordHasher",
     )
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
         }
-    HAYSTACK_CONNECTIONS['default'] = {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+    HAYSTACK_CONNECTIONS["default"] = {
+        "ENGINE": "haystack.backends.simple_backend.SimpleEngine",
         }
