0a3f4c578d7d3500c039b05ceff689871b72ea27
Nader Morshed <morshed.nader@gmail.com>
2015-01-27 16:49:01 -0500

    Corrected rendering issue for archives page

--- base/templatetags/nested_tags.py
+++ base/templatetags/nested_tags.py
@@ -9,10 +9,10 @@ def nested_to_flat(nodes):
     if isinstance(nodes, list):
         yield {'start_nodes': True}
         for node in nodes:
-            yield {'start_node': True}
             for i in nested_to_flat(node):
                 yield i
-            yield {'end_node': True}
         yield {'end_nodes': True}
     else:
+        yield {'start_node': True}
         yield {'data': nodes, 'is_data': True}
+        yield {'end_node': True}
