64e60f23bb9048e338e0bdba274f08810a4b5af7
Nader Morshed <morshed.nader@gmail.com>
2015-01-27 22:07:15 -0500

    Fixed two more typos

--- workshift/signals.py
+++ workshift/signals.py
@@ -120,8 +120,8 @@ def set_week_long(sender, instance, **kwargs):
 def subtract_instance_hours(sender, instance, **kwargs):
     if instance.closed and instance.workshifter:
         pool_hours = instance.workshifter.pool_hours.get(pool=instance.pool)
-        pool_hours -= instance.hours
-        pool_hours.ssave()
+        pool_hours.standing -= instance.hours
+        pool_hours.save()
 
 # TODO: Auto-notify manager and workshifter when they are >= 10 hours down
 # TODO: Auto-email central when workshifters are >= 15 hours down?
