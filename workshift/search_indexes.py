54d9b6bb862fdf3c95c912f9f1e4d7db410afd40
Nader Morshed <morshed.nader@gmail.com>
2015-02-20 01:43:39 -0500

    Use ModelSearchIndex throughout search_indexes

--- workshift/search_indexes.py
+++ workshift/search_indexes.py
@@ -10,67 +10,88 @@ from haystack import indexes
 from workshift.models import Semester, WorkshiftPool, WorkshiftType, \
     WorkshiftProfile, RegularWorkshift, WorkshiftInstance
 
-class SemesterIndex(indexes.SearchIndex, indexes.Indexable):
+
+class SemesterIndex(indexes.ModelSearchIndex, indexes.Indexable):
     """ Index for semesters. """
-    text = indexes.EdgeNgramField(document=True, use_template=True)
     season = indexes.EdgeNgramField(model_attr='season', boost=2)
     year = indexes.EdgeNgramField(model_attr='year', boost=2)
-    start_date = indexes.DateField(model_attr='start_date')
-    end_date = indexes.DateField(model_attr='end_date')
+    class Meta:
+        model = Semester
+        fields = (
+            "start_date",
+            "end_date",
+        )
 
     def get_model(self):
-        return Semester
+        return self.model
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 
-class WorkshiftPoolIndex(indexes.SearchIndex, indexes.Indexable):
+
+class WorkshiftPoolIndex(indexes.ModelSearchIndex, indexes.Indexable):
     """ Index for workshift pools. """
-    text = indexes.EdgeNgramField(document=True, use_template=True)
-    title = indexes.EdgeNgramField(model_attr='title')
-    semester = indexes.EdgeNgramField(model_attr='semester')
+    class Meta:
+        model = WorkshiftPool
+        fields = (
+            "title",
+            "semester",
+        )
 
     def get_model(self):
-        return WorkshiftPool
+        return self.model
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 
-class WorkshiftTypeIndex(indexes.SearchIndex, indexes.Indexable):
+
+class WorkshiftTypeIndex(indexes.ModelSearchIndex, indexes.Indexable):
     """ Index for workshift types. """
-    text = indexes.EdgeNgramField(document=True, use_template=True)
-    title = indexes.EdgeNgramField(model_attr='title')
-    description = indexes.EdgeNgramField(model_attr='description', null=True)
-    quick_tips = indexes.EdgeNgramField(model_attr='quick_tips', null=True)
+    class Meta:
+        model = WorkshiftType
+        fields = (
+            "title",
+            "description",
+            "quick_tips",
+        )
 
     def get_model(self):
-        return WorkshiftType
+        return self.model
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 
-class WorkshiftProfileIndex(indexes.SearchIndex, indexes.Indexable):
+
+class WorkshiftProfileIndex(indexes.ModelSearchIndex, indexes.Indexable):
     """ Index for workshift profiles. """
     text = indexes.EdgeNgramField(document=True, use_template=True)
     user = indexes.EdgeNgramField(model_attr='user', boost=1.5)
     exact_user = indexes.CharField(model_attr='user', faceted=True)
-    semester = indexes.EdgeNgramField(model_attr='semester')
+    class Meta:
+        model = WorkshiftProfile
+        fields = (
+            "semester",
+        )
 
     def get_model(self):
-        return WorkshiftProfile
+        return self.model
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 
-class RegularWorkshiftIndex(indexes.SearchIndex, indexes.Indexable):
+
+class RegularWorkshiftIndex(indexes.ModelSearchIndex, indexes.Indexable):
     """ Index for a regular workshift. """
-    text = indexes.EdgeNgramField(document=True, use_template=True)
-    workshift_type = indexes.EdgeNgramField(model_attr='workshift_type')
-    pool = indexes.EdgeNgramField(model_attr='pool')
-    addendum = indexes.EdgeNgramField(model_attr='addendum', null=True)
+    class Meta:
+        model = RegularWorkshift
+        fields = (
+            "workshift_type",
+            "pool",
+            "addendum",
+        )
 
     def get_model(self):
-        return RegularWorkshift
+        return self.model
 
     def index_queryset(self, using=None):
         return self.get_model().objects.filter(pool__semester__current=True)
@@ -78,7 +99,6 @@ class RegularWorkshiftIndex(indexes.SearchIndex, indexes.Indexable):
 
 class WorkshiftInstanceIndex(indexes.ModelSearchIndex, indexes.Indexable):
     """ Index for a workshift instance. """
-    text = indexes.EdgeNgramField(document=True, use_template=True)
     class Meta:
         model = WorkshiftInstance
         fields = (
@@ -91,7 +111,7 @@ class WorkshiftInstanceIndex(indexes.ModelSearchIndex, indexes.Indexable):
         )
 
     def get_model(self):
-        return WorkshiftInstance
+        return self.model
 
     def index_queryset(self, using=None):
         return self.get_model().objects.filter(semester__current=True)
